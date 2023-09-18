517e7edfa0c9b8238da847d7bc3be86258b177e7
willianctti <willianctti@gmail.com>
2023-09-18 14:59:15 -0300

    Operadores matemáticos  Métodos  	exemplo com smartTv  Escopo  Palavras Reservadas  Java Doc  Estruturas Condicionais  	switch case, Estruturas de repetição, do e do while

---
+++ ForEArray/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        
+        String alunos [] = {"Willian", "Raissa", "Ranya"};
+
+        for(String aluno : alunos) {
+            System.out.println("Nome do aluno: " + aluno);
+        }
+    }
+}
+
+
---
+++ exemploFor/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos [] = {"Willia," "Raissa", "Ranya"};
+
+        for(String aluno : alunos) {
+            System.out.println("Nome do aluno: " + aluno);
+        }
+    }
+}
